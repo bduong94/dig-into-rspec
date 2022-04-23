1ca17b1b4ccfe895b7e10d410bf865cb6154f8af
Brian Duong <brianminhduong@gmail.com>
2022-04-23 05:42:45 +0000

    Create tests for Boat class

--- boat_spec.rb
+++ boat_spec.rb
@@ -4,4 +4,17 @@ describe Boat do
     it 'should create boats' do
         expect(Boat.new).to be_a Boat
     end
+    describe '#allowed_aboard?' do
+        it 'returns true if inventory includes a life jacket' do
+            a_boat = Boat.new
+            allowed = a_boat.allowed_aboard?(['life jacket', 'sun glasses'])
+            expect(allowed).to be true
+        end
+
+        it 'returns false if inventory does not include a life jacket' do
+            a_boat = Boat.new
+            allowed = a_boat.allowed_aboard?(['t-shirt', 'sun glasses'])
+            expect(allowed).to be false
+        end
+    end
 end
